5af27a0506ec01d12ace65a4a5409f9520fec877
ruancs <ruan.cs@hotmail.com>
2023-08-10 05:14:30 -0300

    style:ajustes lp

--- react/sustentabilidade.tsx
+++ react/sustentabilidade.tsx
@@ -3,9 +3,96 @@ import React from 'react'
 const lpsustentabilidade: StorefrontFunctionComponent = () => {
   return (
     <>
+
+      <link rel="stylesheet" href="https://boschferramentasbrasil.vtexassets.com/_v/public/assets/v1/bundle/css/asset.min.css?v=3&files=theme,boschferramentasbrasil.builderio@0.5.1$style.common,boschferramentasbrasil.builderio@0.5.1$style.small,boschferramentasbrasil.builderio@0.5.1$style.notsmall,boschferramentasbrasil.builderio@0.5.1$style.large,boschferramentasbrasil.builderio@0.5.1$style.xlarge&files=fonts,989db2448f309bfdd99b513f37c84b8f5794d2b5&files=npm,animate.css@3.7.0/animate.min.css&files=react~vtex.render-runtime@8.134.0-beta.0,common&files=react~vtex.store-components@3.169.0,Container,4,SearchBar,Logo,DiscountBadge&files=react~vtex.styleguide@9.146.9,0,1,Spinner&files=react~vtex.flex-layout@0.21.3,0&files=react~vtex.admin-pages@4.51.2,HighlightOverlay&files=react~vtex.rich-text@0.16.0,index&files=react~boschferramentasbrasil.components@0.3.0,LgpdCookie,HelpFloater,HeaderZipCode,SearchBarMobileToggle,MenuHeaderMobile&files=react~vtex.login@2.57.0,1&files=react~vtex.minicart@2.67.1,Minicart&files=react~vtex.search@2.16.4,Autocomplete&files=react~vtex.product-summary@2.83.1,ProductSummaryImage&files=overrides,vtex.product-list@0.37.1$overrides,vtex.product-comparison@0.18.0$overrides,vtex.minicart@2.67.1$overrides,boschferramentasbrasil.builderio@0.5.1$overrides,boschferramentasbrasil.builderio@0.5.1$fonts&workspace=master" />
+      <link rel="stylesheet" href="https://boschferramentasbrasil.myvtex.com/files/vtex--app-lp.css" />
+
+
+      <section className='banner__main'>
+
+      </section>
+
+
       <section className='section__text-one'>
         <h1>Sustentabilidade</h1>
-        <p>Na Bosch, temos um compromisso sério com o meio ambiente e acreditamos que todos podem contribuir para tornar a sustentabilidade uma realidade.</p>
+        <p>Na Bosch, temos um compromisso sério com o meio ambiente e acreditamos que<br /><strong>todos podem contribuir para tornar a sustentabilidade uma realidade.</strong></p>
+        <span>Junte-se a nós nessa jornada rumo a um futuro mais verde!</span>
+      </section >
+
+
+      <section className='section__cards'>
+        <div className='card'>
+          <svg xmlns="http://www.w3.org/2000/svg" width="125" height="136" viewBox="0 0 125 136" fill="none">
+            <path d="M124.079 67.685C124.079 105.025 96.2649 135.37 62.0397 135.37C27.8145 135.37 0 105.025 0 67.685C0 30.3454 27.8145 0 62.0397 0C65.8138 0 69.6395 0.394829 73.3102 1.12808C76.102 1.69213 77.9632 4.62514 77.4462 7.67097C76.9292 10.7168 74.2408 12.7473 71.449 12.1833C68.3987 11.5629 65.1934 11.2808 62.0397 11.2808C33.5531 11.2808 10.3399 36.6063 10.3399 67.685C10.3399 98.7637 33.5531 124.089 62.0397 124.089C90.5262 124.089 113.739 98.7637 113.739 67.685C113.739 64.2444 113.429 60.8037 112.912 57.4194C112.395 54.3736 114.256 51.4406 117.048 50.8766C119.892 50.3125 122.528 52.3431 123.045 55.3889C123.717 59.3936 124.079 63.5675 124.079 67.685ZM66.4858 34.1809C69.3293 34.6322 71.9143 32.4888 72.3279 29.3866C72.7415 26.2843 70.7769 23.4641 67.9334 23.0129C65.9688 22.7309 64.0043 22.5617 62.0397 22.5617C39.2401 22.5617 20.6799 42.8108 20.6799 67.685C20.6799 92.5592 39.2401 112.808 62.0397 112.808C84.8392 112.808 103.399 92.5592 103.399 67.685C103.399 65.5416 103.244 63.3983 102.986 61.2549C102.572 58.1527 99.9873 56.0093 97.1438 56.4606C94.3003 56.9118 92.3357 59.732 92.7493 62.8342C92.9561 64.4136 93.0595 66.0493 93.0595 67.685C93.0595 86.3548 79.1523 101.528 62.0397 101.528C44.9271 101.528 31.0198 86.3548 31.0198 67.685C31.0198 49.0152 44.9271 33.8425 62.0397 33.8425C63.539 33.8425 65.0383 33.9553 66.4858 34.1809ZM55.5772 51.3278C53.6643 49.0152 50.4072 48.7896 48.2875 50.8766C43.8931 55.1633 41.4115 61.3113 41.4115 67.685C41.4115 80.1503 50.6657 90.2467 62.0914 90.2467C67.9851 90.2467 73.5687 87.4829 77.4979 82.7449C79.4108 80.4324 79.204 76.8789 77.0843 74.7919C74.9646 72.705 71.7075 72.9306 69.7946 75.2432C67.83 77.6121 65.0383 79.0222 62.0914 79.0222C56.4044 79.0222 51.7514 73.9459 51.7514 67.7414C51.7514 64.5264 52.9922 61.4805 55.2153 59.3372C57.335 57.2502 57.5418 53.6968 55.6289 51.3842L55.5772 51.3278ZM63.5907 58.0963C61.5744 60.2961 61.5744 63.8495 63.5907 66.0493C64.6247 67.1774 65.9172 67.685 67.2613 67.685C68.6055 67.685 69.898 67.121 70.932 66.0493L95.2826 39.4829H108.673C110.069 39.4829 111.361 38.8625 112.343 37.8472L122.683 26.5664C124.183 24.9306 124.596 22.5053 123.821 20.4183C123.045 18.3314 121.132 16.9213 119.064 16.9213H108.725V5.64042C108.725 3.38425 107.484 1.2973 105.519 0.451233C103.555 -0.451233 101.383 0.0564043 99.8839 1.69213L89.5439 12.973C88.5616 14.0446 88.0446 15.4547 88.0446 16.9777V31.5863L63.6941 58.1527L63.5907 58.0963Z" fill="#CEE741" />
+          </svg>
+
+          <p>A estratégia de sustentabilidade da Bosch é trabalhada há muitos anos, e nosso principal objetivo é manter o compromisso de sermos uma empresa sustentável.</p>
+
+
+        </div>
+
+        <div className='card'>
+          <svg xmlns="http://www.w3.org/2000/svg" width="123" height="122" viewBox="0 0 123 122" fill="none">
+            <g clip-path="url(#clip0_107_165)">
+              <path d="M117.101 44.6732C114.095 41.9342 110.219 40.5547 106.146 40.7214C102.083 40.9135 98.343 42.6771 95.604 45.6788L79.3522 63.5377C76.5677 58.9441 71.5194 55.8615 65.7635 55.8615H15.9467C7.5883 55.8615 0.786377 62.6635 0.786377 71.0218V106.396C0.786377 114.754 7.5883 121.556 15.9467 121.556H68.745L118.208 66.0442C123.731 59.8436 123.231 50.2573 117.101 44.6732ZM110.663 59.3231L64.2171 111.449H15.9467C13.1622 111.449 10.8933 109.185 10.8933 106.396V71.0218C10.8933 68.2323 13.1622 65.9684 15.9467 65.9684H65.7635C68.9472 65.9684 71.5345 68.5608 71.5345 71.7445C71.5345 74.5946 69.407 77.0557 66.5771 77.4549L40.4963 81.1793L41.9264 91.1851L68.0022 87.4607C73.94 86.6117 78.766 82.4729 80.7116 77.0556L103.078 52.4757C103.998 51.4701 105.251 50.8788 106.615 50.8131C107.99 50.7576 109.284 51.2225 110.294 52.1422C112.351 54.017 112.518 57.2411 110.663 59.3231ZM43.7406 45.7395C48.4656 45.7395 52.9075 43.9 56.2479 40.5597C63.1458 33.6618 63.1408 22.4381 56.2074 15.5048L43.7406 3.32092L31.2333 15.5502C24.3354 22.4431 24.3354 33.6719 31.2333 40.5648C34.5737 43.9051 39.0156 45.7395 43.7406 45.7395ZM38.3435 22.7261L43.7406 17.4453L49.1023 22.6857C52.0586 25.642 52.0586 30.4528 49.1023 33.4091C46.2471 36.2693 41.2493 36.2693 38.384 33.4091C35.4277 30.4528 35.4277 25.642 38.3435 22.7261Z" fill="#CEE741" />
+            </g>
+            <defs>
+              <clipPath id="clip0_107_165">
+                <rect x="0.786377" y="0.278809" width="121.283" height="121.283" rx="12" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
+
+          <p>Acreditamos que é possível fazer a diferença, agindo com responsabilidade e preocupação com o meio ambiente, sem abrir mão da excelência em nossos produtos.</p>
+
+        </div>
+
+      </section>
+
+      <section className='section__history'>
+        <h2>Conheça nossa história sustentável</h2>
+        <hr></hr>
+        <ul className='history__years'>
+          <li>
+            <label>2007</label>
+
+          </li>
+
+          <li>
+            <label>2016</label>
+
+          </li>
+
+          <li>
+            <label>2018</label>
+
+          </li>
+
+          <li>
+            <label>2019</label>
+
+          </li>
+
+          <li>
+            <label>2020</label>
+
+          </li>
+
+          <li>
+            <label>2021</label>
+
+          </li>
+
+          <li>
+            <label>2022</label>
+
+
+          </li>
+
+
+
+        </ul>
+
       </section>
     </>
   )
