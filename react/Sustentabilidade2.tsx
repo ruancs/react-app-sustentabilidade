774e708b069c77b12d6aa3f116035bc206517332
ruancs <ruan.cs@hotmail.com>
2023-08-22 02:26:12 -0300

    projeto dividido para inserir prateleira de produto

---
+++ react/Sustentabilidade2.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Accordion from './accordion';
+
+
+
+const lpsustentabilidade2: StorefrontFunctionComponent = () => {
+
+  return (
+    <>
+      <section className="section__faq">
+        <Accordion />
+
+      </section>
+
+      <section className="banner__blog desktop"></section>
+      <section className="banner__blog mobile">
+        <img src="https://boschferramentasbrasil.vteximg.com.br/arquivos/banner_blog_sustentabilidade_mob.jpg" alt="banner_main_mobile" />
+      </section>
+
+    </>
+  )
+
+}
+
+export default lpsustentabilidade2
